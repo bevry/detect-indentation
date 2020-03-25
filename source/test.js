eb05ba62fa64ca1842c98315f215fe686669557d
Benjamin Lupton <b@lupton.cc>
2020-03-26 09:25:10 +1100

    v4.0.0 - boundation, changelog, version bump

--- source/test.js
+++ source/test.js
@@ -4,126 +4,126 @@ const kava = require('kava')
 const { equal, errorEqual } = require('assert-helpers')
 const detectIndentation = require('./')
 
-kava.suite('detect-indentation', function(suite, test) {
+kava.suite('detect-indentation', function (suite, test) {
 	const tests = [
 		{
 			name: 'none',
 			input: 'a\nb',
-			output: null
+			output: null,
 		},
 		{
 			name: 'spaces: 0, 4',
 			input: 'a\n    b',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 0, 4, 8',
 			input: 'a\n    b\n        c',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 4, 4',
 			input: '    a\n    b',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 4, 8',
 			input: '    a\n        b',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 4, 8, 12',
 			input: '    a\n        b\n            c',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 4, 0, 4',
 			input: '    a\nb\n    c',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 8, 4, 8',
 			input: '        a\n    b\n        c',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'spaces: 4, 8, 4',
 			input: '    a\n        b\n    c',
-			output: '    '
+			output: '    ',
 		},
 		{
 			name: 'tabs: 0, 1',
 			input: 'a\n\tb',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 0, 1, 2',
 			input: 'a\n\tb\n\t\tc',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 1, 1',
 			input: '\ta\n\tb',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 1, 2',
 			input: '\ta\n\t\tb',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 1, 2, 3',
 			input: '\ta\n\t\tb\n\t\t\tc',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 1, 2, 1',
 			input: '\ta\n\t\tb\n\tc',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 1, 0, 1',
 			input: '\ta\nb\n\tc',
-			output: '\t'
+			output: '\t',
 		},
 		{
 			name: 'tabs: 2, 1, 2',
 			input: '\t\ta\n\tb\n\t\tc',
-			output: '\t'
-		}
+			output: '\t',
+		},
 	]
 	const errors = [
 		{
 			name: 'uneven: 2s, 3s',
 			input: '  a\n   b',
-			output: 'indentation is uneven: [  ] of size [2] vs [   ] of size [3]'
+			output: 'indentation is uneven: [  ] of size [2] vs [   ] of size [3]',
 		},
 		{
 			name: 'uneven: 2t, 3t',
 			input: '\t\ta\n\t\t\tb',
 			output:
-				'indentation is uneven: [\t\t] of size [2] vs [\t\t\t] of size [3]'
+				'indentation is uneven: [\t\t] of size [2] vs [\t\t\t] of size [3]',
 		},
 		{
 			name: 'mixed: 4s, 2t',
 			input: '    a\n\t\tb',
-			output: 'mixed spaces and tabs'
+			output: 'mixed spaces and tabs',
 		},
 		{
 			name: 'mixed: 2t, 4s',
 			input: '    a\n\t\tb',
-			output: 'mixed spaces and tabs'
-		}
+			output: 'mixed spaces and tabs',
+		},
 	]
 
-	tests.forEach(function({ name, input, output }) {
-		test(name, function() {
+	tests.forEach(function ({ name, input, output }) {
+		test(name, function () {
 			equal(detectIndentation(input), output, name)
 		})
 	})
 
-	errors.forEach(function({ name, input, output }) {
-		test(name, function() {
+	errors.forEach(function ({ name, input, output }) {
+		test(name, function () {
 			try {
 				detectIndentation(input)
 				throw new Error('test should have failed but it did not')
